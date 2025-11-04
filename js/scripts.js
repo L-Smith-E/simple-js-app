3a2f83c47ae4c9b6871d84fc948e818275266da6
Laurence Smith <33005711+L-Smith-E@users.noreply.github.com>
2025-11-04 14:36:17 -0500

    Encapsulate pokemonList in pokemonRepo IIFE
    Refactored the global pokemonList array into a pokemonRepo IIFE with add and getAll methods for better encapsulation. Updated code to use pokemonRepo.getAll() and replaced the for loop with forEach for improved readability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-//Array of pokemon objects
-pokemonList = [
-  {
+let pokemonRepo = (function () {
+
+  let pokemonList = [
+    {
     "name": "Eevee",
     "types": ["Grass", "Poison"],
     "height": 0.3
@@ -25,18 +26,51 @@ pokemonList = [
     "types": ["Electric"],
     "height": 0.4
   }
-]
+  ];
+
+  function add(pokemon) {
+    if ((typeof pokemon === "object") /*&& Object.keys(pokemon).includes("name") && Object.keys(pokemon).includes("height")*/) {
+    pokemonList.push(pokemon);
+    } 
+  }   
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    
+  };
+})();
+//Array of pokemon objects
+// pokemonList = [
+//   
+// ]
 //Setting the height threshold and label
+
 let tallestHeight = 0.5;
 let label = " - Wow, that’s big!";
-
 //Looping through the array of objects
-for (let i = 0; i < pokemonList.length; i++) {
-//conditional
-   if (pokemonList[i].height > tallestHeight)
-  {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + label + "<br><br>");
+pokemonRepo.getAll().forEach(function(pokemon){ 
+  //conditional
+  if (pokemon.height > tallestHeight) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ")" + label + "<br><br>");
+  } else {
+    document.write(pokemon.name + " (height: " + pokemon.height + ")" + "<br><br>");
   }
-  else
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br><br>");
-}
+});
+
+
+
+
+
+// for (let i = 0; i < pokemonList.length; i++) {
+// //conditional
+//    if (pokemonList[i].height > tallestHeight)
+//   {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + label + "<br><br>");
+//   }
+//   else
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br><br>");
+// }
